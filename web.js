64ec0340f980f7d36929aa9ecf85caadfc793018
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2020-12-27 08:27:00 +0530

    hehe

--- web.js
+++ web.js
@@ -3,40 +3,6 @@ var mysql = require('mysql');
 var app = express();
 app.use(express.logger());
 
-var db_config = {
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: 'b6d6c6e8740d20',
-    password: 'b3f75ada',
-    database: 'heroku_1daa39da0375291'
-};
-
-var connection;
-
-function handleDisconnect() {
-    console.log('1. connecting to db:');
-    // connection = mysql.createConnection(db_config); // Recreate the connection, since
-	// 												// the old one cannot be reused.
-
-    // connection.connect(function(err) {              	// The server is either down
-    //     if (err) {                                     // or restarting (takes a while sometimes).
-    //         console.log('2. error when connecting to db:', err);
-    //         setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
-    //     }                                     	// to avoid a hot loop, and to allow our node script to
-    // });                                     	// process asynchronous requests in the meantime.
-    // 											// If you're also serving http, display a 503 error.
-    // connection.on('error', function(err) {
-    //     console.log('3. db error', err);
-    //     if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
-    //         handleDisconnect();                      	// lost due to either server restart, or a
-    //     } else {                                      	// connnection idle timeout (the wait_timeout
-    //         throw err;                                  // server variable configures this)
-    //     }
-    // });
-
-}
-
-handleDisconnect();
-
 app.get('/example', function(request, response) {
     // connection.query('SELECT * from t_users', function(err, rows, fields) {
     //     if (err) {
@@ -45,7 +11,7 @@ app.get('/example', function(request, response) {
     //     }
     //     response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
     // });
-    response.json({"message":'Hello World!!!! HOLA MUNDO!!!!'});
+    response.send({"message":'Hello World!!!! HOLA MUNDO!!!!'});
     console.log("hello");
 });
 
